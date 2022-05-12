2ab747556a3b794185b6596cc306c779789b3366
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-12 15:07:59 -0300

    Variaveis

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,15 @@
+const myName = "Luis";
+const birthCity = "Umuarama";
+let birthYear = 1992;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+//birthCity = "Jacupiranga" não pode mudar valor de "const"
+
+console.log(birthCity);
