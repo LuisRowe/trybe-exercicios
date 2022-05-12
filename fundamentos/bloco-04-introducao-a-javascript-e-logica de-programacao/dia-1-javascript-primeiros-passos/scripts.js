df3e26c4cb436cded6266f13cd208acde3961dd5
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-12 15:30:23 -0300

    Final segundo exercício

--- fundamentos/bloco-04-introducao-a-javascript-e-logica de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -13,3 +13,35 @@ console.log(birthYear);
 //birthCity = "Jacupiranga" não pode mudar valor de "const"
 
 console.log(birthCity);
+
+//Final da primeira parte
+
+let patientId = 50;
+let isEnrolled = true; // verificar tipo variaveis: string, number, boolean ...
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+
+
+console.log(typeof patientAge);
+
+//Exercicio 2 parte 1 finalizado
+
+const base = 5;
+const height = 8; //base e altura retangulo calcular area e perimetro.
+
+const area = base * height;
+const perimeter = 2 * base + 2 * height;
+
+console.log(area);
+console.log(perimeter);
+
+// Final exercício 2
+
