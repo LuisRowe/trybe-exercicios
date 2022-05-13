1f91d2187fda52a619d6663d29305e7857c49fc4
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-13 15:33:03 -0300

    Exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio/scripts.js
@@ -8,10 +8,24 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //2
 
+// let resultado = 0;
+
+// for (let index = 0; index < numbers.length; index += 1){
+//   resultado = numbers[index] + resultado;
+// }
+
+// console.log(resultado);
+
+//3
+
 let resultado = 0;
 
 for (let index = 0; index < numbers.length; index += 1){
   resultado = numbers[index] + resultado;
 }
 
-console.log(resultado);
+let media = resultado / numbers.length;
+
+console.log(media);
+
+//4
