ba946d6b3a30c4e19d75ae415bc464c66b10fd2a
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-13 15:25:55 -0300

    Conteudo For-of

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/conteudo/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/conteudo/scripts.js
@@ -27,3 +27,11 @@
 // for (let index = 0; index < groceryList.length; index += 1){
 //   console.log(groceryList[index]);
 // }
+
+//Primeiro-For-of
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+// for (let nome of names) {
+//   console.log("O nome é: " + nome);
+// }
