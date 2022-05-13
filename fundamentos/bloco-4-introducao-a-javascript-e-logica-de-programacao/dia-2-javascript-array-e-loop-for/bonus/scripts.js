f5651afb460f049835c63727c31887b843a634f7
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-13 18:08:18 -0300

    Exercicio bonus 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus/scripts.js
@@ -2,11 +2,41 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //Bonus 1 - Ordenar array menor ao maior.
 
+// let cofre = null;
+
+// for (let contador = 0; contador < numbers.length; contador +=1){
+//   for (let index = 0; index < numbers.length; index +=1){
+//     if (numbers[index] > numbers[index+1]){
+//       cofre = numbers[index+1];
+//       numbers[index+1] = numbers[index];
+//       numbers[index]= cofre;
+//     }
+//   }
+// }
+// console.log(numbers);
+
+//Bonus 2 - Ordenar array menor ao maior.
+
+// let cofre = null;
+
+// for (let contador = 0; contador < numbers.length; contador +=1){
+//   for (let index = 0; index < numbers.length; index +=1){
+//     if (numbers[index] < numbers[index+1]){
+//       cofre = numbers[index+1];
+//       numbers[index+1] = numbers[index];
+//       numbers[index]= cofre;
+//     }
+//   }
+// }
+// console.log(numbers);
+
+//Bonus 3
+
 let cofre = null;
 
 for (let contador = 0; contador < numbers.length; contador +=1){
   for (let index = 0; index < numbers.length; index +=1){
-    if (numbers[index] > numbers[index+1]){
+    if (numbers[index] < numbers[index+1]){
       cofre = numbers[index+1];
       numbers[index+1] = numbers[index];
       numbers[index]= cofre;
@@ -14,5 +44,3 @@ for (let contador = 0; contador < numbers.length; contador +=1){
   }
 }
 console.log(numbers);
-
-//Bonus 1 - Ordenar array maior ao menor.
