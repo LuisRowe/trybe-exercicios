07173bed8aedf239c40c82a0e8b115651e524146
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-13 18:12:11 -0300

    Exercicio bonus 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus/scripts.js
@@ -30,17 +30,15 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 // console.log(numbers);
 
-//Bonus 3
+//Bonus 3 - multiplicar um pelo outro.
 
 let cofre = null;
 
-for (let contador = 0; contador < numbers.length; contador +=1){
-  for (let index = 0; index < numbers.length; index +=1){
-    if (numbers[index] < numbers[index+1]){
-      cofre = numbers[index+1];
-      numbers[index+1] = numbers[index];
-      numbers[index]= cofre;
-    }
+for (let index = 0; index < numbers.length; index += 1){
+  if (index + 1 == numbers.length){
+    numbers[index] = numbers[index] * 2
+  } else {
+    numbers[index] = numbers[index] * numbers[index + 1]
   }
 }
 console.log(numbers);
