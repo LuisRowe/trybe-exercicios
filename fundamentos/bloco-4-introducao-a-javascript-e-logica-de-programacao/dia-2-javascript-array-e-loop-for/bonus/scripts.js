e874bb3d86745b0664c35f38ce7f8faa22d0e362
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-13 16:23:14 -0300

    Exercicio bonus 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bonus/scripts.js
@@ -1,5 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (let index = 0; index < numbers.length; index +=1){
-  
+//Bonus 1 - Ordenar array menor ao maior.
+
+let cofre = null;
+
+for (let contador = 0; contador < numbers.length; contador +=1){
+  for (let index = 0; index < numbers.length; index +=1){
+    if (numbers[index] > numbers[index+1]){
+      cofre = numbers[index+1];
+      numbers[index+1] = numbers[index];
+      numbers[index]= cofre;
+    }
+  }
 }
+console.log(numbers);
+
+//Bonus 1 - Ordenar array maior ao menor.
