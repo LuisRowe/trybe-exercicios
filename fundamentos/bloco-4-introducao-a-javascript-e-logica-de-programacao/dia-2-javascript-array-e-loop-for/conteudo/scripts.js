d07345a2a75ced4c0b810f163c7fbf9e641d9f30
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-13 15:08:48 -0300

    Conteudo arrays

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/conteudo/scripts.js
@@ -0,0 +1,21 @@
+//Primeiro-Arrays
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf("Serviços");
+
+console.log(menuServices);
+
+//Segundo-Arrays
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf("Portfólio");
+
+console.log(indexOfPortfolio);
+
+//Terceiro-Arrays
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push("Contato");
+
+console.log(menu);
