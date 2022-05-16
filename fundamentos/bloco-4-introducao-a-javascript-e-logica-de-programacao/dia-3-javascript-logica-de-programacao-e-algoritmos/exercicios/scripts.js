abd6c7fda5d7baf713397ebbe8d92829f89a5bde
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-16 18:57:53 -0300

    Exercicios 4.3 1 e 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/scripts.js
@@ -0,0 +1,24 @@
+//1 - Fatorial
+
+// let numero = 10;
+// let fatorial = 1;
+
+// for (let index = numero; index > 0; index -= 1) {
+//   fatorial = index * fatorial;
+//   console.log (fatorial);
+// }
+
+// console.log(fatorial);
+
+//2 - Inverter palavra
+
+// let word = 'tryber';
+// let trocada = [];
+
+// for (let index = word.length-1; index >= 0; index -= 1){
+//   console.log(index);
+//   trocada.push(word[index]);
+// }
+
+// trocada = trocada.join("") // join junta o array.
+// console.log(trocada);
