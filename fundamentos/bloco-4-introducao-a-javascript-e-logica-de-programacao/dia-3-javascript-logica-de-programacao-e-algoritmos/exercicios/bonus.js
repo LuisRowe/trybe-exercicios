801b61c885a56ac339aefc689577ee04a94beb82
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-16 19:52:52 -0300

    Primeiro bonus 4.3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/bonus.js
@@ -0,0 +1,13 @@
+let estrela = "*";
+let estrelaNaLinha = "";
+
+let linha = 3;
+
+for (let index = 0; index < linha; index += 1) {
+  estrelaNaLinha += estrela;
+}
+
+for (let index = 0; index < linha; index += 1) {
+  console.log(estrelaNaLinha);
+}
+
