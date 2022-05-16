f140a4c01ac3927c62402a8f1a17ed6d401e1700
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-16 19:56:07 -0300

    Segundo bonus 4.3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/bonus.js
@@ -1,13 +1,31 @@
+//1 - Quadrado de asterisco
+
+// let estrela = "*";
+// let estrelaNaLinha = "";
+
+// let linha = 3;
+
+// for (let index = 0; index < linha; index += 1) {
+//   estrelaNaLinha += estrela;
+// }
+
+// for (let index = 0; index < linha; index += 1) {
+//   console.log(estrelaNaLinha);
+// }
+
+// 2 Triangulo - Base N
+
 let estrela = "*";
 let estrelaNaLinha = "";
 
-let linha = 3;
+let linha = 5;
+
+// for (let index = 0; index < linha; index += 1) {
+//   estrelaNaLinha += estrela;
+// }
 
 for (let index = 0; index < linha; index += 1) {
   estrelaNaLinha += estrela;
-}
-
-for (let index = 0; index < linha; index += 1) {
   console.log(estrelaNaLinha);
 }
 
