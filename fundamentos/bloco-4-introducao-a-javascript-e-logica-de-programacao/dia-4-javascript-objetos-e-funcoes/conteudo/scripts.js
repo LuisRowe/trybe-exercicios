264d8f60f03f9c1efb9f89f232a2e855c7691e19
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-17 15:46:19 -0300

    Conteudo 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/conteudo/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/conteudo/scripts.js
@@ -1,35 +1,16 @@
-//2 - quando a chave é número só funciona colchetes diasDaSemana['1'], e não "diasDaSemana.1".
+//Para fixa objetos
 
-// let diasDaSemana = {
-//   1: 'domingo',
-//   2: 'segunda',
-//   3: 'terça',
-//   4: 'quarta',
-//   5: 'quinta',
-//   6: 'sexta',
-//   7: 'sábado',
-//   };
-  
-//   // console.log(diasDaSemana.1); // SyntaxError: Unexpected number
-//   console.log(diasDaSemana['1']); // domingo
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
 
-  //3 - Conta banco
-
-  let conta = {
-    agencia: '0000',
-    banco: {
-      cod: '012',
-      id: 4,
-      nome: 'TrybeBank',
-    },
-  };
-  
-  let infoDoBanco = 'banco';
-  console.log(conta[infoDoBanco]); // { cod: '012', id: 4, nome: 'TrybeBank' }
-  console.log(conta[infoDoBanco]['nome']); // TrybeBank
-  
-  console.log(conta.agencia); // 0000
-  console.log(conta['agencia']); // 0000
-  
-  console.log(conta.banco.cod); // 012
-  console.log(conta['banco']['id']); // 4
+let objeto = {
+  name: "Marta",
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  }
+}
