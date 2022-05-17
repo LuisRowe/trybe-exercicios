153d702ed1079a843c4b06abdbcdc59ace751c6a
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-05-17 15:53:48 -0300

    Conteudo 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/conteudo/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/conteudo/scripts.js
@@ -12,5 +12,12 @@ let objeto = {
   medals: {
     golden: 2,
     silver: 3,
-  }
+  },
+  bestInTheWorld: [2006,2007,2008,2009,2010,2018],
 }
+
+console.log("A jogadora " + objeto.name + " " + objeto.lastName + " tem " + objeto.age + " anos de idade.")
+
+console.log("A jogadora " + objeto.name + " " + objeto.lastName + " foi eleita a melhor do mundo por " + objeto.bestInTheWorld.length + " vezes.")
+
+console.log
