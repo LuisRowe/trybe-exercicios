abc0cde2d5351f90e27c23955e6a4f07114ff5f8
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-06-08 15:35:28 -0300

    Exercicios Parte 2 exe 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/script2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/script2.js
@@ -1,5 +1,29 @@
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+let oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
+function ordenar(array) {
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+  let arrumar = [];
+  let guardado = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    for (let index2 = 0; index2 < array.length; index2 += 1) {
+      guardado = 0;
+      if (array[index2] > array[index2 + 1]) {
+        guardado = array[index2];
+        array[index2] = array[index2 + 1];
+        array[index2 + 1] = guardado;
+      }
+    }
+  }
+}
+
+ordenar(oddsAndEvens);
+
+console.log(`Os números ${oddsAndEvens} se encontram ordebados de forma crescente!`); // será necessário alterar essa linha 😉
+
+// Utilizando array.sort
+
+let oddsAndEvensSort = [13, 3, 4, 10, 7, 2];
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+oddsAndEvensSort = oddsAndEvensSort.sort((a, b) => a - b); 
+
+console.log(oddsAndEvensSort);
