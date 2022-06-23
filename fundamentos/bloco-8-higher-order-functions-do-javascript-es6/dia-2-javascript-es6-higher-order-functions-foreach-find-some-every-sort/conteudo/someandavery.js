89faa4156aa30b783504010a49d43f65f4a12490
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-06-23 15:54:54 -0300

    Finalizando o conteúdo 8.2

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/conteudo/someandavery.js
@@ -0,0 +1,46 @@
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('X', listNames)); // false
+
+const grades = {
+  portugues: 'Aprovado',
+  matematica: 'Reprovado',
+  ingles: 'Aprovado',
+};
+
+const verifyGrades = (studentGrades) => (
+  Object.values(studentGrades).every((grade) => grade === 'Aprovado')
+);
+
+console.log(verifyGrades(grades));
+
+//Para fixar Procurar nomes no array
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  //Adicione seu código aqui
+  return arr.some((busca) => busca === name);
+}
+
+console.log(hasName(names, 'Ana'))
+
+//Buscar se todos são maiores de idade
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  //Adicione seu código aqui
+  return arr.every((buscar) => buscar.age >= 18);
+}
+
+console.log(verifyAges(people, 18));
