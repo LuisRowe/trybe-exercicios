fdb879604df023b0ef26f38a7bf5e3640c59c628
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2022-07-18 13:12:36 -0300

    up

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-3/tests/fetchCharacter.test.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-3/tests/fetchCharacter.test.js
@@ -2,7 +2,25 @@ require('../mocks/fetchSimulator');
 const { fetchCharacter } = require('../src/fetchCharacter');
 
 describe('Teste a função fetchCharacter', () => {
-  it('', async () => {
-    
+  it('Verifica se o nome da personagem é Wonder Woman', async () => {
+    const character = await fetchCharacter('720');
+    expect(character.name)
+      .toEqual('Wonder Woman')
+  });
+
+  it('Verifica se retorna erro ao executar a função sem parâmetro', async () => {
+    expect(await fetchCharacter()).toEqual(new Error('You must provide an url'))
+  });
+
+  it("Verifica se retorna 'Invalid id' ao executar a função com parâmetro que não existe", async () => {
+    const character = await fetchCharacter('id');
+    expect(character).toEqual('Invalid id')
+  });
+
+  it('Verifica se fetch é chamada com o endpoint correto', async () => {
+    const url = 'https://www.superheroapi.com/api.php/4192484924171229/720';
+    await fetchCharacter('720');
+    expect(fetch).toHaveBeenCalledTimes(4);
+    expect(fetch).toHaveBeenCalledWith(url);
   });
 });
