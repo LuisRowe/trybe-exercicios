0d3ed1c135fe4e54965d62302fe0a894febe9456
Luis Rowe <luisfelipe.apoloniorowe@gmail.com>
2022-10-26 09:49:07 -0300

    Configurações do conteudo do dia 4.2

--- back-end/secao-4-Introdução ao desenvolvimento Web com Node.js/Dia 01: Node.js: Runtime Assíncrono/conteudo/hello-world/index.js
+++ back-end/secao-4-Introdução ao desenvolvimento Web com Node.js/Dia 01: Node.js: Runtime Assíncrono/conteudo/hello-world/index.js
@@ -5,14 +5,27 @@
 
 // console.log(`Beleza ${nome}? Tu tem ${idade} anos? Ta velho heim?`);
 
+// const fs = require('fs').promises;
+
+// async function main() {
+//   try {
+//     const data = await fs.readFile('./meu-arquivo.txt', 'utf-8');
+//     console.log(data);
+//   } catch (err) {
+//     console.error(`Erro ao ler o arquivo: ${err.message}`);
+//   }
+// }
+
+// main()
+
 const fs = require('fs').promises;
 
 async function main() {
   try {
-    const data = await fs.readFile('./meu-arquivo.txt', 'utf-8');
-    console.log(data);
+    await fs.writeFile('./meu-arquivo.txt', 'Meu textão');
+    console.log('Arquivo escrito com sucesso!');
   } catch (err) {
-    console.error(`Erro ao ler o arquivo: ${err.message}`);
+    console.error(`Erro ao escrever o arquivo: ${err.message}`);
   }
 }
 
