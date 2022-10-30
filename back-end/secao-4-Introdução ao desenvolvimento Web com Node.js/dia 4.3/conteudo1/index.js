7c4862ee75dfb7503e5327c6ddb61ac79f9187ea
Luis Rowe <luisfelipe.apoloniorowe@gmail.com>
2022-10-30 12:24:03 -0300

    Instalando lint

--- back-end/secao-4-Introdução ao desenvolvimento Web com Node.js/dia 4.3/conteudo1/index.js
+++ back-end/secao-4-Introdução ao desenvolvimento Web com Node.js/dia 4.3/conteudo1/index.js
@@ -4,7 +4,7 @@ console.log('Quando a média for menor que 7, retorna "reprovação":');
 
 const respostaCenario1 = calculaSituacao(4);
 if (respostaCenario1 === 'reprovação') {
-  console.log(`Ok 🚀`);
+  console.log('Ok 🚀');
 } else {
   console.error('Resposta não esperada 🚨');
 }
@@ -16,7 +16,7 @@ console.log('Quando a média for maior que 7, retorna "aprovação":');
 
 const respostaCenario2 = calculaSituacao(9);
 if (respostaCenario2 === 'aprovação') {
-  console.log(`Ok 🚀`);
+  console.log('Ok 🚀');
 } else {
   console.error('Resposta não esperada 🚨');
 }
@@ -28,7 +28,7 @@ console.log('Quando a média for igual a 7, retorna "aprovação":');
 
 const respostaCenario3 = calculaSituacao(7);
 if (respostaCenario3 === 'aprovação') {
-  console.log(`Ok 🚀`);
+  console.log('Ok 🚀');
 } else {
   console.error('Resposta não esperada 🚨');
 }
