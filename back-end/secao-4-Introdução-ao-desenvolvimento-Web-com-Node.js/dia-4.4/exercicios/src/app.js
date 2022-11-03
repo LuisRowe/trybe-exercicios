83aa1b946a6b1c270052f2b7ec32a34faf776488
Luis Rowe <luisfelipe.apoloniorowe@gmail.com>
2022-11-03 13:26:15 -0300

    Final ex do dia 4.4

--- back-end/secao-4-Introdução-ao-desenvolvimento-Web-com-Node.js/dia-4.4/exercicios/src/app.js
+++ back-end/secao-4-Introdução-ao-desenvolvimento-Web-com-Node.js/dia-4.4/exercicios/src/app.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const generateToken = require('./utils/generateToken');
 const validateCreatedAt = require('./middlewares/validateCratedAt');
 const validateDescription = require('./middlewares/validateDescription');
 const validateDifficulty = require('./middlewares/validateDifficulty');
 const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
 const validateRating = require('./middlewares/validateRating');
+const validateUser = require('./middlewares/validateUser');
+const auth = require('./middlewares/auth');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/activities',
+  auth,
   validateName, 
   validatePrice, 
   validateDescription, 
@@ -20,5 +24,10 @@ app.post('/activities',
   (_req, res) => {
   res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
 });
+
+app.post('/signup', validateUser, (req, res) => {
+  const token = generateToken();
+  res.status(200).json({ token });
+})
 
 module.exports = app;
