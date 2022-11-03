af6077ac29bfc2783c317f7b12bce37e84f978ba
Luis Rowe <luisfelipe.apoloniorowe@gmail.com>
2022-11-03 11:03:35 -0300

    Ex 1 ao 3

---
+++ back-end/secao-4-Introdução-ao-desenvolvimento-Web-com-Node.js/dia-4.4/exercicios/src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/activities', (req, res) => {
+  res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
+});
+
+module.exports = app;
