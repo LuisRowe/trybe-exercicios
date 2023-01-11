c80464ac1769bc5944b0b55a5ff0afebcde4b373
LuisRowe <luisfelipe.apoloniorowe@gmail.com>
2023-01-11 10:52:15 -0300

    Iniciando 9.1

---
+++ back-end/secao-9/dia-9.1/conteudo/index.ts
@@ -0,0 +1,16 @@
+interface Email {
+  rem: string;
+  dest: string;
+  assunto: string;
+  mensagem: string;
+}
+
+// Email to stringfy
+
+// ---
+
+class Cachorros {
+  constructor(parameters) {
+    
+  }
+}
